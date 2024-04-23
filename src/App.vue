9b78bfd4512bce77d310c0161475c1d1fbd875d8
meguriri <1310500986@qq.com>
2024-04-23 17:07:33 +0800

    reset

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,8 @@
 <template >
-  <!-- <login/> -->
-  <div class="mb-4">
-    <el-button @click="fic" :icon="Edit">Default</el-button>
-    <el-button >Primary</el-button>
-    <el-button type="success" :loading="ll" round>Success</el-button>
-    <el-button type="info"  circle>Info</el-button>
-    <el-button type="warning" size="small">Warning</el-button>
-    <el-button color="#626aef"  size="large">Danger</el-button>
-  </div>
-  <div >
-    1111
-  </div>
+  
 </template>
 
 <script setup>
-  import login from "./components/login/login.vue"
-  import {Edit} from '@element-plus/icons-vue'
-  import {ref} from 'vue'
-  let ll =ref(false)
-  function fic(){
-    ll.value=!ll.value
-    console.log(ll.value)
-  }
+ 
 </script>
 
--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,17 +1,7 @@
 <template>
-    <alret :type="alretType" :message="message"/>
-    <former @submit="callback"/>
+    
 </template>
 
 <script setup>
-    import alret from "./alert.vue"
-    import former from "./former.vue"
-    import {ref} from "vue"
-    let alretType =ref(0)
-    let message =ref('')
-    function callback(res){
-        console.log("login",res)
-        alretType.value=res.type
-        message.value=res.msg
-    }
+    
 </script>
